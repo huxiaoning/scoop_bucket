c5a6aeb2fef0242c005a3af45a953a89e0143d40
huxiaoningsworld <huxiaoningsworld@sina.com>
2026-01-13 11:05:15 +0800

    fix

--- hxn_scripts/hello.ps1
+++
@@ -1,3 +0,0 @@
-$name = split-path (whoami) -leaf
-"Hello, $name!"
-
--- hxn_scripts/mitm.ps1
+++
@@ -1,4 +0,0 @@
-$inbound = @("--mode", "socks5", "--listen-host", "0.0.0.0", "--listen-port", "38080")
-$web = @("--web-host", "127.0.0.1", "--web-port", "38081")
-$outbound = @("--upstream", "socks5://127.0.0.1:7890")
-mitmweb @inbound @web @outbound
